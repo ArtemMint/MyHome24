6196bc1fe2758d3a7a28e37976738762cf8b8bbe
Artem Yurchak <nemnovus@gmail.com>
2021-05-16 18:11:38 +0300

    Remove pay_company.py

--- apps/admin_panel/models/__init__.py
+++ apps/admin_panel/models/__init__.py
@@ -5,7 +5,7 @@ from admin_panel.models.manage_site.page_contacts import *
 from admin_panel.models.manage_site.page_services import *
 
 # Pay-company
-from admin_panel.models.pay_company.pay_company import PayCompany
+from admin_panel.models.pay_company import PayCompany
 
 # Services
 from admin_panel.models.services.services import Services
