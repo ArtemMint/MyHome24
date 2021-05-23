4a5b960f07f41f2be94b78ccf5dadf609b4abca1
Artem Yurchak <nemnovus@gmail.com>
2021-05-23 21:40:03 +0300

    Created CRUD views.

--- apps/admin_panel/views/master_request.py
+++ apps/admin_panel/views/master_request.py
@@ -1,9 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
-from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+from admin_panel import models
 
-@method_decorator(login_required(login_url='/admin/site/login'), name='dispatch')
-class MasterRequestsList(TemplateView):
-    template_name = "admin_panel/master_request/index.html"
+
+@login_required(login_url='/admin/site/login')
+def master_requests_list(request):
+    return render(
+        request,
+        "admin_panel/master_request/index.html",
+        {
+            'master_requests_list': models.MasterRequest.get_queryset_list(),
+        }
+    )
+
+
+@login_required(login_url='/admin/site/login')
+def master_request_create(request):
+    return render(
+        request,
+        "admin_panel/master_request/index.html",
+        {
+            'master_requests_list': models.MasterRequest.get_queryset_list(),
+        }
+    )
+
+
+@login_required(login_url='/admin/site/login')
+def master_request_update(request, pk):
+    return render(
+        request,
+        "admin_panel/master_request/index.html",
+        {
+            'master_requests_list': models.MasterRequest.get_queryset_list(),
+        }
+    )
+
+
+@login_required(login_url='/admin/site/login')
+def master_request_detail(request, pk):
+    return render(
+        request,
+        "admin_panel/master_request/index.html",
+        {
+            'master_requests_list': models.MasterRequest.get_queryset_list(),
+        }
+    )
+
+
+@login_required(login_url='/admin/site/login')
+def master_request_delete(request, pk):
+    return render(
+        request,
+        "admin_panel/master_request/index.html",
+        {
+            'master_requests_list': models.MasterRequest.get_queryset_list(),
+        }
+    )
