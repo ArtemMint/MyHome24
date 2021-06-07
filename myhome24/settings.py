7b75ffb5251204aea4bdc04fda5e29b719a40a0a
Artem Yurchak <nemnovus@gmail.com>
2021-06-07 12:05:30 +0300

    Update to deploy.

--- myhome24/settings.py
+++ myhome24/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'myhome24.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": 'django.db.backends.postgresql',
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
         "NAME": 'myhome24_db',
         "USER": 'artemmint',
         "PASSWORD": 12345,
