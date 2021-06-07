3343bd212855516acde49c26fc5b85d00359a68e
Artem Yurchak <nemnovus@gmail.com>
2021-06-07 12:51:32 +0300

    Update to deploy.

--- myhome24/settings.py
+++ myhome24/settings.py
@@ -136,12 +136,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
-STATIC_URL = '/staticfiles/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+STATIC_URL = "/staticfiles/"
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
