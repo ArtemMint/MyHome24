a09707d51d5b26d00bb0551e2e6a749b5265a52e
Artem Yurchak <nemnovus@gmail.com>
2021-06-19 11:04:30 +0300

    Updated Account templates.

--- apps/admin_panel/forms/account.py
+++ apps/admin_panel/forms/account.py
@@ -1,6 +1,9 @@
 from django import forms
+import django_filters
+from django_filters import DateFilter, ModelChoiceFilter
 
 from admin_panel import models
+from register import models as register_models
 
 
 class AccountForm(forms.ModelForm):
@@ -49,3 +52,28 @@ class AccountForm(forms.ModelForm):
                 }
             ),
         }
+
+
+# class AccountFilter(django_filters.FilterSet):
+#     owner = ModelChoiceFilter(
+#         queryset=register_models.User.get_active_users()
+#     )
+#     created_date = DateFilter(
+#         widget=forms.DateInput(
+#             attrs={
+#                 'type': 'date',
+#                 'class': 'datepicker',
+#             }
+#         )
+#     )
+#
+#     class Meta:
+#         model = models.AccountTransaction
+#         fields = (
+#             'number',
+#             'status',
+#             'house',
+#             'section',
+#             'flat',
+#             'created_date',
+#         )
