282111927fa41169f93d5b10f38e0ed76f7d4d5b
Artem Yurchak <nemnovus@gmail.com>
2021-06-25 10:55:17 +0300

    Added filtering to MasterRequest list.

--- apps/admin_panel/forms/master_request.py
+++ apps/admin_panel/forms/master_request.py
@@ -1,3 +1,4 @@
+import django_filters
 from django import forms
 from datetime import datetime
 
@@ -98,3 +99,101 @@ class MasterRequestForm(forms.ModelForm):
                 }
             ),
         }
+
+
+class MasterRequestFilterForm(django_filters.FilterSet):
+    STATUS = (
+        ('Новое', 'Новое'),
+        ('В работе', 'В работе'),
+        ('Выполнено', 'Выполнено'),
+    )
+    MASTER_TYPE = (
+        ('Любой специалист', 'Любой специалист'),
+        ('Сантехник', 'Сантехник'),
+        ('Слесарь', 'Слесарь'),
+        ('Электрик', 'Электрик'),
+    )
+
+    id = django_filters.Filter(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        )
+    )
+    created_date = django_filters.DateFilter(
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'class': ' form-control daterangepicker',
+                'onchange': 'form.submit();',
+            }
+        )
+    )
+    master_type = django_filters.ChoiceFilter(
+        choices=MASTER_TYPE,
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        ),
+    )
+    description = django_filters.Filter(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        )
+    )
+    flat = django_filters.ModelChoiceFilter(
+        queryset=models.Flat.get_flats_list(),
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        )
+    )
+    owner = django_filters.ModelChoiceFilter(
+        queryset=register_models.User.get_active_users(),
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        )
+    )
+    master = django_filters.ModelChoiceFilter(
+        queryset=register_models.User.users_admin.get_queryset(),
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        )
+    )
+    status = django_filters.ChoiceFilter(
+        choices=STATUS,
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+                'onchange': 'form.submit();',
+            }
+        ),
+    )
+
+    class Meta:
+        model = models.MasterRequest
+        fields = (
+            'id',
+            'created_date',
+            'owner',
+            'description',
+            'flat',
+            'master_type',
+            'status',
+            'master',
+        )
