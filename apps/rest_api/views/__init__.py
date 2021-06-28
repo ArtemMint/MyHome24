e7809f01fe2d2906d7c8cb73d00115c2de3d0577
Artem Yurchak <nemnovus@gmail.com>
2021-06-28 11:19:12 +0300

    Added get user role view.

--- apps/rest_api/views/__init__.py
+++ apps/rest_api/views/__init__.py
@@ -2,4 +2,5 @@ from rest_api.views.flat import *
 from rest_api.views.house import *
 from rest_api.views.api_overview import *
 from rest_api.views.statistic import *
+from rest_api.views.user import *
 
