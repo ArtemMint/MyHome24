76db4cc931c75ca8caf2e3e31cdd7371424e19aa
Artem Yurchak <nemnovus@gmail.com>
2021-07-02 14:02:53 +0300

    Created test suite to User/UserAdmin login.

--- apps/register/tests/test_views.py
+++ apps/register/tests/test_views.py
@@ -15,13 +15,37 @@ class UserLoginTest(TestCase):
     def tearDown(self) -> None:
         self.user.delete()
 
-    def test_login(self):
+    def test_login_view(self):
         response = self.client.post(
             reverse_lazy('personal_cabinet:user_login'),
             self.user_data,
             follow=True,
         )
         self.assertTrue(response.context['user'].is_active)
+
+    def test_login_view_wrong_password(self):
+        user_data = {
+            'email': 'user@gmail.com',
+            'password': 'secret!23',
+        }
+        response = self.client.post(
+            reverse_lazy('admin_panel:admin_login'),
+            user_data,
+            follow=True,
+        )
+        self.assertEqual(response.context['error_message'], 'Email or password not correct.')
+
+    def test_login_view_wrong_email(self):
+        user_data = {
+            'email': 'us@gmail.com',
+            'password': 'secret',
+        }
+        response = self.client.post(
+            reverse_lazy('admin_panel:admin_login'),
+            user_data,
+            follow=True,
+        )
+        self.assertEqual(response.context['error_message'], 'Email or password not correct.')
 
 
 class UserAdminLoginTest(TestCase):
@@ -35,10 +59,34 @@ class UserAdminLoginTest(TestCase):
     def tearDown(self) -> None:
         self.user.delete()
 
-    def test_login(self):
+    def test_login_view(self):
         response = self.client.post(
             reverse_lazy('admin_panel:admin_login'),
             self.user_data,
             follow=True,
         )
         self.assertTrue(response.context['user'].is_active)
+
+    def test_login_view_wrong_password(self):
+        user_data = {
+            'email': 'admin@gmail.com',
+            'password': 'Secret1',
+        }
+        response = self.client.post(
+            reverse_lazy('admin_panel:admin_login'),
+            user_data,
+            follow=True,
+        )
+        self.assertEqual(response.context['error_message'], 'Email or password not correct.')
+
+    def test_login_view_wrong_email(self):
+        user_data = {
+            'email': 'admi@gmail.com',
+            'password': 'secret',
+        }
+        response = self.client.post(
+            reverse_lazy('admin_panel:admin_login'),
+            user_data,
+            follow=True,
+        )
+        self.assertEqual(response.context['error_message'], 'Email or password not correct.')
