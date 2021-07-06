a61dea11e9386c2c56592a5a87e28ca3c8f63fe5
Artem Yurchak <nemnovus@gmail.com>
2021-07-06 16:46:36 +0300

    Added new context for flats.

--- apps/personal_cabinet/views/profile.py
+++ apps/personal_cabinet/views/profile.py
@@ -1,13 +1,19 @@
 from django.views import generic
 
-from register import models, views
+from admin_panel import models
+from register import models as reg_models, views
 
 
 class UserProfileView(generic.DetailView):
-    model = models.User
+    model = reg_models.User
     context_object_name = 'user'
     template_name = 'personal_cabinet/profile/view.html'
 
     def get_object(self, queryset=None):
         user_id = self.request.user.pk
         return self.model.objects.get(pk=user_id)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data(**kwargs)
+        context['flat_list'] = models.Flat.objects.filter(owner=self.get_object())
+        return context
