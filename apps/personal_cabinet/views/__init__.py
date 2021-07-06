620517d53544360ec53d4dfe6f89318b6049386b
Artem Yurchak <nemnovus@gmail.com>
2021-07-06 13:03:16 +0300

    Added user_profile url and refactored some code.

---
+++ apps/personal_cabinet/views/__init__.py
@@ -0,0 +1,6 @@
+from personal_cabinet.views.tariff import *
+from personal_cabinet.views.invoice import *
+from personal_cabinet.views.message import *
+from personal_cabinet.views.master_request import *
+from personal_cabinet.views.profile import *
+from personal_cabinet.views.summary import *
