3ea3e5d86bbe3a752fbd7aaeb50f8d7bd744b998
Artem Yurchak <nemnovus@gmail.com>
2021-07-09 11:10:05 +0300

    Created MasterRequest create view to User cabinet.

--- apps/personal_cabinet/views/master_request.py
+++ apps/personal_cabinet/views/master_request.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from admin_panel import models, views
+from personal_cabinet import forms
 
 
 class MasterRequestListView(generic.ListView):
@@ -14,6 +15,38 @@ class MasterRequestListView(generic.ListView):
         user = self.request.user.id
         user_requests = self.model.objects.filter(owner=user)
         return user_requests
+
+
+class MasterRequestCreateView(generic.View):
+    model = models.MasterRequest
+    form_class = forms.UserMasterRequestForm
+    template_name = 'personal_cabinet/master_request/create.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return self.render_template(form)
+
+    def post(self, request):
+        user = request.user
+        form = self.form_class(
+            request.POST,
+        )
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = user
+            obj.save()
+            return redirect('personal_cabinet:master_request_list')
+        else:
+            return self.render_template(form)
+
+    def render_template(self, form):
+        return render(
+            self.request,
+            self.template_name,
+            {
+                'form': form,
+            }
+        )
 
 
 class MasterRequestDeleteView(generic.DeleteView):
