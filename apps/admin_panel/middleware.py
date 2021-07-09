9682edd70dfbbf08e4c1208fba939bed7d830f04
Artem Yurchak <nemnovus@gmail.com>
2021-07-09 11:08:47 +0300

    Added separate sessions to admin and user accounts.

---
+++ apps/admin_panel/middleware.py
@@ -0,0 +1,75 @@
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.conf import settings
+from django.contrib.sessions.backends.base import UpdateError
+from django.core.exceptions import SuspiciousOperation
+from django.utils.cache import patch_vary_headers
+from django.utils.http import http_date
+
+import time
+
+
+def resolve_request_to_session_cookie_name(request):
+    if request.path.startswith('/admin'):
+        return f'admin_{settings.SESSION_COOKIE_NAME}'
+    else:
+        return f'client_{settings.SESSION_COOKIE_NAME}'
+
+
+class AdminSessionMiddleware(SessionMiddleware):
+    def process_request(self, request):
+        cookie_name = resolve_request_to_session_cookie_name(request)
+        session_key = request.COOKIES.get(cookie_name)
+        request.session = self.SessionStore(session_key)
+
+    def process_response(self, request, response):
+        """
+        If request.session was modified, or if the configuration is to save the
+        session every time, save the changes and set a session cookie or delete
+        the session cookie if the session has been emptied.
+        """
+        try:
+            accessed = request.session.accessed
+            modified = request.session.modified
+            empty = request.session.is_empty()
+        except AttributeError:
+            pass
+        else:
+            cookie_name = resolve_request_to_session_cookie_name(request)
+            if cookie_name in request.COOKIES and empty:
+                response.delete_cookie(
+                    cookie_name,
+                    path=settings.SESSION_COOKIE_PATH,
+                    domain=settings.SESSION_COOKIE_DOMAIN,
+                )
+            else:
+                if accessed:
+                    patch_vary_headers(response, ("Cookie",))
+                if (modified or settings.SESSION_SAVE_EVERY_REQUEST) and not empty:
+                    if request.session.get_expire_at_browser_close():
+                        max_age = None
+                        expires = None
+                    else:
+                        max_age = request.session.get_expiry_age()
+                        expires_time = time.time() + max_age
+                        expires = http_date(expires_time)
+                    if response.status_code != 500:
+                        try:
+                            request.session.save()
+                        except UpdateError:
+                            raise SuspiciousOperation(
+                                "The request's session was deleted before the "
+                                "request completed. The user may have logged "
+                                "out in a concurrent request, for example."
+                            )
+                        response.set_cookie(
+                            cookie_name,
+                            request.session.session_key,
+                            max_age=max_age,
+                            expires=expires,
+                            domain=settings.SESSION_COOKIE_DOMAIN,
+                            path=settings.SESSION_COOKIE_PATH,
+                            secure=settings.SESSION_COOKIE_SECURE or None,
+                            httponly=settings.SESSION_COOKIE_HTTPONLY or None,
+                            samesite=settings.SESSION_COOKIE_SAMESITE,
+                        )
+        return response
